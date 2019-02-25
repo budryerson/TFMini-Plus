cb350097ee799e46ff5dc06889849b1e1d794d4a
Bud Ryerson <bud@budryerson.com>
2019-02-25 12:04:47 -0800

    Add files via upload

--- TFMPlus/example/TFMP_example.ino
+++ TFMPlus/example/TFMP_example.ino
@@ -1,15 +1,22 @@
 /* File Name: bene07.ino
  * Developer: Bud Ryerson
  * Inception: 29 JAN 2019
- * Last work: 21 FEB 2019
- *
- * Added temperature conversion
+ * Last work: 25 FEB 2019
  *
  * Description: Arduino sketch to test the Benewake TFMini Plus
  * time-of-flight Lidar ranging sensor using the TFMPlus library.
  *
  * Default settings for the TFMini Plus are a 115200 serial baud rate
- * and a 100Hz measurement frame rate.
+ * and a 100Hz measurement frame rate. The device will begin returning
+ * measurement data right away:
+ * Distance in centimeters,
+ * signal strength in arbitrary units,
+ * and an encoded number for temperature in degrees centigrade.
+ *
+ * Use the 'buildCommand' to send commands and return a status code.
+ * Commands are selected from the library's list of defined commands.
+ * Parameters can be entered directly (115200, 250, etc) but for
+ * safety, they should be chosen from the library's defined lists.
  *
  * NOTE: The TFMini Plus is a unique product and the various Arduino
  * Libraries for the TFMini are NOT compatible with the TFMini Plus.
@@ -23,43 +30,45 @@ TFMPlus tfmP;         // Create a TFMini Plus object
 //#include <SoftwareSerial.h>      // untested software serial library
 //SoftwareSerial mySerial(10, 11); // RX, TX Pins
 
-uint16_t tfDist;       // Distance measurement variable
+uint16_t tfDist;       // Distance measurement in centimeters (default)
 uint16_t tfFlux;       // Luminous flux or intensity of return signal
-uint16_t tfTemp;       // Temperature in hundreths of a degrees Centigrade
-uint16_t loopCount;    // Loop counter
-
+uint16_t tfTemp;       // Temperature in degrees Centigrade (coded)
+uint16_t loopCount;    // Loop counter (1-20)
 
 void setup()
 {
   Serial.begin( 115200);   // Intialize terminal serial port
   delay(20);               // Give port time to initalize
   printf_begin();          // Initialize printf.
-  printf("\r\nTFMPlus Device Library - 21FEB2019\r\n");
+  printf("\r\nTFMPlus Device Library - 25FEB2019\r\n");  // say 'hello'
 
-  Serial1.begin( 115200);   // Initialize device serial port.
-  delay(20);                // Give port time to initalize
+  Serial1.begin( 115200);  // Initialize device serial port.
+  delay(20);               // Give port time to initalize
   while( !Serial1.available())
   {
       printf("Device serial port \"Serial1\" not available.\r\n");
-      printf("Wait 3 seconds, and check again .\r\n");
+      printf("Will wait 3 seconds and try again .\r\n");
       delay( 3000);      // Delay 3 seconds
   }
   tfmP.begin( &Serial1); // Initialize device library object and...
                          // pass device serial port to the object.
+
   tfDist = 0;            // Clear device data variables.
   tfFlux = 0;
   tfTemp = 0;
   loopCount = 0;         // Reset loop counter.
+
   delay(500);            // And wait half a second.
 
-  // Try five times to Get Firmware Version, passing back...
-  // the version number (firmVer) as a three byte array.
-  uint8_t frmVer[ 3];   // Three byte firmware version
+  // Try five times to Get the firmware version number
+  // as 'tfmp.version', a three byte array.
   for( uint8_t fvi = 1; fvi < 6; ++fvi)
   {
-	  if( tfmP.getFirmwareVersion( frmVer))
+    tfmP.status = tfmP.buildCommand( OBTAIN_FIRMWARE_VERSION, 0);
+	  if( tfmP.status == READY)
 	  {
-		  printf( "Firmware Version: %1u.%1u.%1u\r\n", frmVer[ 0], frmVer[ 1], frmVer[ 2]);
+		  printf( "Firmware Version: %1u.%1u.%1u\r\n",
+		    tfmP.version[ 0], tfmP.version[ 1], tfmP.version[ 2]);
       break;  // Break out of loop if successful.
 	  }
 	  else
@@ -78,17 +87,18 @@ void setup()
 void loop()
 {
   printf( "Loop:%02u", loopCount);           // Display the loop count.
-  if( tfmP.getData( tfDist, tfFlux, tfTemp)) // Get data from device.
+  tfmP.status = tfmP.getData( tfDist, tfFlux, tfTemp); // Get data from device.
+  if( tfmP.status == READY)
   {
     printf( " Dist:%04u", tfDist);          // Display the distance.
-    if( loopCount >= 20)                     // And every twenty loops...
+    if( loopCount >= 20)                    // And every twenty loops...
     {
       printf( " Flux:%05u", tfFlux);        // display signal strength...
-      uint8_t tfTempC = uint8_t(( tfTemp / 8) - 256);
-      printf( " Temp:% 2uC", tfTempC);      // display temperature as °C...
+      uint8_t tfTempC = uint8_t(( tfTemp / 8) - 256); // decode temperature...
+      printf( " Temp:% 2uC", tfTempC);      // display temp as °C...
       uint8_t tfTempF = uint8_t( tfTempC * 9.0 / 5.0) + 32.0;
       printf( "% 3uF", tfTempF);            // and again as °F.
-      loopCount = 0;                        // Reset the loop counter.
+      loopCount = 0;                        // Reset loop counter.
     }
   }
   else tfmP.printStatus();
--- TFMPlus/src/TFMPlus.h
+++ TFMPlus/src/TFMPlus.h
@@ -3,8 +3,26 @@
  *            The TFMini Plus is a unique product, and the various
  *            TFMini Libraries are not compatible with the Plus.
  * Developer: Bud Ryerson
- * Inception: 31 JAN 2019
- * Last work: 21 FEB 2019 - added future struct code
+ * Inception: 31 JAN 2019 v0.1.0
+ * Last work: 25 FEB 2019 v0.2.0
+ *
+ * Description: Arduino library for the Benewake TFMini Plus
+ * time-of-flight Lidar ranging sensor.
+ *
+ * Default settings for the TFMini Plus are a 115200 serial baud rate
+ * and a 100Hz measurement frame rate. The device will begin returning
+ * measurement data right away:
+ * Distance in centimeters,
+ * signal strength in arbitrary units,
+ * and an encoded number for temperature in degrees centigrade.
+ *
+ * Use the 'buildCommand' to send commands and return a status code.
+ * Commands are selected from the library's list of defined commands.
+ * Parameters can be entered directly (115200, 250, etc) but for
+ * safety, they should be chosen from the library's defined lists.
+ *
+ * NOTE: The TFMini Plus is a unique product and the various Arduino
+ * Libraries for the TFMini are NOT compatible with the TFMini Plus.
  *
  * Inspired by the TFMini Arduino Library of Peter Jansen (11 DEC 2017)
  */
@@ -15,24 +33,23 @@
 #include <Arduino.h>    // Always remember to include this. It is important.
 
 // Defines
-#define TFMPLUS_BAUDRATE   115200
-#define TFMPLUS_DEBUGMODE  1
 
-// The frame size is nominally 9 characters, but don't include
-// the first two 0x59's that mark the start of the frame
+// The output data frame size is 9 bytes
 #define TFMPLUS_FRAME_SIZE           9
 
-// Timeout Limits
+// Timeout Limits for various functions
 #define MAX_READS_BEFORE_SERIAL     20
 #define MAX_BYTES_BEFORE_HEADER     10
 #define MAX_ATTEMPTS_TO_MEASURE     20
 
-// System Status
+// System Status and Error Condition
 #define READY                 0
 #define SERIAL                1
 #define HEADER                2
 #define CHECKSUM              3
 #define TIMEOUT               4
+#define PASS                  5
+#define FAIL                  6
 #define MEASUREMENT_OK       10
 
 /* - - - - - - - - -  TFMini Plus  - - - - - - - - -
@@ -44,8 +61,48 @@
   Command format:
   Byte0  Byte1   Byte2   Byte3 to Len-2  Byte Len-1
   0x5A   Length  Cmd ID  Payload if any   Checksum
-  - - - - - - - - - - - - - - - - - - - - - - - - - */
+ - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+// Commands: one byte payload, command number, command length  
+#define    OBTAIN_FIRMWARE_VERSION    0x000104   // returns 3 byte firmware version
+#define    TRIGGER_DETECTION          0x000404   // must set frame rate to zero first
+                                                 // returns a 9 byte data frame
+#define    SYSTEM_RESET               0x000204   // returns a 1 byte pass/fail (0/1)
+#define    RESTORE_FACTORY_SETTINGS   0x000A04   //           "
+#define    SAVE_SETTINGS              0x000B04   // must follow every command to modify parameters
+                                                 // also returns a 1 byte pass/fail (0/1)
+#define    SET_FRAME_RATE             0x000306   // returns echo of command
+#define    SET_BAUD_RATE              0x000608   //           "
+#define    STANDARD_FORMAT_CM         0x010505   //           "
+#define    PIXHAWK_FORMAT             0x020505   //           "
+#define    STANDARD_FORMAT_MM         0x060505   //           "
+#define    ENABLE_OUTPUT              0x000705   //           "
+#define    DISABLE_OUTPUT             0x010705   //           "
+
+// Command Parameters
+#define    BAUD_9600          0x002580   // expression of baud rate
+#define    BAUD 14400         0x003840   // in hexidecimal
+#define    BAUD_19200         0x004B00
+#define    BAUD_56000         0x00DAC0
+#define    BAUD_115200        0x01C200
+#define    BAUD_460800        0x070800
+#define    BAUD_921600        0x0E1000
+
+#define    FRAME_0            0x0000  // expression of frame rate
+#define    FRAME_1            0x0001  // as hex number
+#define    FRAME_2            0x0002
+#define    FRAME_5            0x0003
+#define    FRAME_10           0x000A
+#define    FRAME_20           0x0014
+#define    FRAME_25           0x0019
+#define    FRAME_50           0x0032
+#define    FRAME_100          0x0064
+#define    FRAME_125          0x007D
+#define    FRAME_200          0x00C8
+#define    FRAME_250          0x00FA
+#define    FRAME_500          0x01F4
+#define    FRAME_1000         0x03E8
+  
 // Object Class Definitions
 class TFMPlus
 {
@@ -53,53 +110,39 @@ class TFMPlus
     TFMPlus();
     ~TFMPlus();
 
-    bool begin( Stream* streamPtr);
-    bool getData( uint16_t& dist, uint16_t& flux, uint16_t& temp);
-    void printStatus();
+    uint8_t version[ 3];   // firmware version
+    uint8_t status;        // system or error status
 
-    // Command function calls
-    bool setSystemReset();
-    bool getFirmwareVersion( uint8_t *data);  // send pointer to return reply data
-    bool setStandardOutput();
-    // Commands function not yet implemented
-    //    bool factoryReset();
-    //    uint16_t setFrameRate( uint16_t frameRate);
-    //    uint32_t setBaudRate( uint32_t baudRate);
-    //    void enableDataOutput();
-    //    void disableDataOutput();
-    //    void externalTrigger();  // set frame rate to zero first
-    //    bool saveSettings();     // must follow every command that modifies parameters
+    bool begin( Stream *streamPtr);   // pass reference to the serial port
+    void printStatus();
+    uint8_t buildCommand( uint32_t cmnd, uint32_t param);
+    uint8_t getData( uint16_t &dist, uint16_t &flux, uint16_t &temp);
 
   private:
-    Stream* pStream;
-    int status;
+    Stream* pStream;    // pointer to the device serial stream
 
-    bool readData( uint8_t &rdByte);
-    bool flushReadBuffer();
-    bool testSum( uint8_t *data, uint8_t len);     // test the checksum byte of the data frame
-    uint8_t makeSum( uint8_t *data, uint8_t len);  // make a checksum byte for the command
-    void putCommand( uint8_t *data, uint8_t len);
-    bool getCommandReply( uint8_t *data, uint8_t len);
-    void printData( uint8_t *data, uint8_t len);
+    bool readData( uint8_t &rdByte);             // try MAX_READS_BEFORE_SERIAL times
+    bool testSum( uint8_t *data, uint8_t len);   // test the checksum byte of the data frame
+    uint8_t makeSum( uint8_t *data);             // make a checksum byte for command data
+    void putCommand( uint8_t *data);
+    uint8_t getResponse();
+    void printData( uint8_t *data, uint8_t len); // for test - display hex characters
 
-    // List of Commnad data by Command Number
-    // 1) Obtain Firmware Version
-    uint8_t getFirmVer[ 4] =  { 0x5A, 0x04, 0x01, 0x5F};
-    // 2) Reset to System Settings
-    uint8_t systemReset[ 4] = { 0x5A, 0x04, 0x02, 0x60};
-    // 3) Set Frame Rate to 0
-    uint8_t frmRate000[ 6] =  { 0x5A, 0x06, 0x03, 0x00, 0x00, 0x63};
-    // 3 Command to Set Frame Rate to 100
-    uint8_t frmRate100[ 6] =  { 0x5A, 0x06, 0x03, 0x64, 0x00, 0xC7};
-    // 5 Command to Set Standard Output Mode
-    uint8_t stdOutMode[ 5] =  { 0x5A, 0x05, 0x05, 0x01, 0x65};
-    // 7 Command to Disable/Enable Data Output
-    uint8_t disableOutput[ 5] = { 0x5A, 0x05, 0x07, 0x00, 0x66};
-    uint8_t  enableOutput[ 5] = { 0x5A, 0x05, 0x07, 0x01, 0x66};
-    // 10 Command to Reset device to Factory Settings
-    uint8_t factoryReset[ 4] = { 0x5A, 0x04, 0x10, 0x6E};
-    // 11 Command to Save Parameters - Must follow every command to modify parameters!
-    uint8_t saveParam[ 4] =   { 0x5A, 0x04, 0x11, 0x6F};
+    struct dataStruct
+    {
+      union
+      {
+        uint8_t data[ 9];
+        struct
+        {
+          uint8_t head[ 2];
+          uint8_t dist[ 2];
+          uint8_t flux[ 2];
+          uint8_t temp[ 2];
+          uint8_t chck;
+        };
+      };
+	  };
 
 };
 
