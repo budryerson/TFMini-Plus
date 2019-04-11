32e9568809f72fc07448d3e3a4d09766653ac001
Bud Ryerson <bud@budryerson.com>
2019-04-11 09:07:23 -0700

    Eample file updated for version 1.3.1
    And outdated example file was in the example folder. My apologies.

--- example/TFMP_example.ino
+++ example/TFMP_example.ino
@@ -1,43 +1,34 @@
 /* File Name: TFMP_example.ino
  * Developer: Bud Ryerson
  * Inception: 29 JAN 2019
- * Last work: 14 MAR 2019
+ * Last work: 08 APR 2019
  *
  * Description: Arduino sketch to test the Benewake TFMini Plus
- * Lidar ranging sensor using the TFMPlus library v1.1.0.
+ * time-of-flight Lidar ranging sensor using the TFMPlus library.
  *
  * Default settings for the TFMini Plus are a 115200 serial baud rate
- * and a 100Hz measurement frame rate.
- *
- * 'begin()' function passes a serial stream to library and
- *  returns TRUE/FALSE whether serial data is available.
- *  Function also sets a public one byte status code.
- *  Status codes are defined within the library.
- *
- * 'getData( dist, flux, temp)' passes back measuremnent data
- *  • dist = distance in centimeters,
- *  • flux = signal strength in arbitrary units, and
- *  • temp = an encoded number in degrees centigrade
- *  Function returns TRUE/FALSE whether completed without error.
- *  Error, if any, is saved as a one byte 'status' code.
- *
- * 'buildCommand( cmnd, param)' sends a 32bit command code (cmnd)
- *  and a 32bit parameter value (param). Returns TRUE/FALSE and
- *  sets a one byte status code.
- *  Commands are selected from the Library's list of defined commands.
- *  Parameter values can be entered directly (115200, 250, etc) but
- *  for safety, they should be chosen from the library's defined lists.
- *  An incorrect value can render the device uncomminicative.
+ * and a 100Hz measurement frame rate. The device will begin returning
+ * measurement data right away:
+ *   Distance in centimeters,
+ *   Signal strength in arbitrary units,
+ *   and an encoded number for Temperature in degrees centigrade.
+
+      **********************     IMPORTANT    ************************
+      ****  Changed name of 'buildCommand()' to 'sendCommand()'.  ****
+      ****************************************************************
+
+ * Use the 'sendCommand' to send commands and return a status code.
+ * Commands are selected from the library's list of defined commands.
+ * Parameters can be entered directly (115200, 250, etc) but for
+ * safety, they should be chosen from the library's defined lists.
  *
  */
 
-#include <TFMPlus.h>  // Add the TFMini-Plus Library
-TFMPlus tfmP;         // Create a TFMini-Plus object
+#include <TFMPlus.h>  // Add the TFMini Plus Library
+TFMPlus tfmP;         // Create a TFMini Plus object
 
 #include "printf.h"
 
-// - - - Software Serial Library was tested with the Arduino - - - 
-// - - - ProMini and worked correctly at a 115200 baud rate. - - -
 //#include <SoftwareSerial.h>      // alternative software serial library
 //SoftwareSerial mySerial(10, 11); // RX, TX Pins
 
@@ -46,99 +37,143 @@ uint16_t tfFlux;       // Luminous flux or intensity of return signal
 uint16_t tfTemp;       // Temperature in degrees Centigrade (coded)
 uint16_t loopCount;    // Loop counter (1-20)
 
-void setup()
+/*  - - -   A couple of useful Lidar commands    - - -  /
+/ - - -   Using the 'sendCommand()' function:   - - -  */
+
+// Try five times to get the firmware version number
+// which is saved as 'tfmp.version', a three byte array.
+void firmwareVersion()
 {
-  Serial.begin( 115200);   // Intialize terminal serial port
-  delay(20);               // Give port time to initalize
-  printf_begin();          // Initialize printf.
-  printf( "\r\n");  
-  printf("TFMPlus Device Library v1.1.1 - 14MAR2019");  // say 'hello'
-  printf( "\r\n");
-
-  //  setup TFMini-Plus Lidar sensor
-  Serial1.begin( 115200);  // Initialize device serial port.
-  delay(20);               // Give port time to initalize
-
-  // Initialize lidar device object and pass serial port.
-  // Display message whether Lidar device is available
-  printf("Lidar device serial port \"Serial1\" ");
-  if( bbTfmp.begin( &Serial1)) printf( "initalized.");
-    else printf( "not available.");
-  printf( "\r\n");
-
-  tfDist = 0;            // Clear device data variables.
-  tfFlux = 0;
-  tfTemp = 0;
-  loopCount = 0;         // Reset loop counter.
-
-  delay(500);            // And wait half a second.
-
-// - - - -  Examples of using the 'buildCommand()' function  - - - -
-
-  // Try five times to get the firmware version number
-  // which is saved as 'tfmp.version', a three byte array.
-  for( uint8_t fvi = 1; fvi < 6; ++fvi)
-  {
-    if( tfmP.buildCommand( OBTAIN_FIRMWARE_VERSION, 0))
-	  {
-      // If successful, display the version number...
-		  printf( "Firmware Version: %1u.%1u.%1u\r\n",
-		    tfmP.version[ 0], tfmP.version[ 1], tfmP.version[ 2]);
-      break;      // and brreak out of loop.
-	  }
-	  else
-	  {
-      // If not successful, display the attempt number
-      // and the error: HEADER, CHERCKSUM, SERIAL, tec.
-	    printf( "Get Firmware Version failed. "); // Display error message...
-	    printf( "Attempt: %u", fvi);              // attempt number..
-      tfmP.printStatus();                       // and error status.
-      printf("\r\n");
-	  }
-    delay(100);  // Wait to try again
-  }
-
-      if( bbTfmp.buildCommand( RESTORE_FACTORY_SETTINGS, 0) == 5)
+    for( uint8_t fvi = 1; fvi < 6; ++fvi)
+    {
+        if( tfmP.sendCommand( OBTAIN_FIRMWARE_VERSION, 0))
         {
-          printf( "Lidar device system reset pass.");
+            // If successful, display the version number...
+            printf( "Lidar firmware version: %1u.%1u.%1u\r\n",
+                tfmP.version[ 0], tfmP.version[ 1], tfmP.version[ 2]);
+            break;      // and brreak out of loop.
         }
-        else printf( "Lidar device system reset fail.");
-        printf( "\r\n");
-
-
-/*      if( bbTfmp.buildCommand( SET_FRAME_RATE, FRAME_20))
+        else
         {
-          printf( "Lidar device frame rate set to 20Hz.");
+            // If not successful, display the attempt number
+            // and the error: HEADER, CHERCKSUM, SERIAL, tec.
+            printf( "Get firmware version failed. "); // Display error message...
+            printf( "Attempt: %u", fvi);              // attempt number..
+            tfmP.printStatus( false);                 // and error status.
+            printf("\r\n");
         }
-        else printf( "Lidar device frame rate command failed.");
-        printf( " Status = %2u", bbTfmp.status);
-        printf( "\r\n");
-*/
-
-  delay( 500);   // And wait another half a second to begin loop
+        delay(100);  // Wait to try again
+    }
 }
 
+void factoryReset()
+{
+    printf( "Lidar factory reset ");
+    if( tfmP.sendCommand( RESTORE_FACTORY_SETTINGS, 0))
+    {
+        printf( "passed.\r\n");
+    }
+    else
+    {
+        printf( "failed.");
+        tfmP.printStatus( false);
+    }
+}
+
+void frameRate( uint16_t rate)
+{
+    printf( "Lidar frame rate ");
+    if( tfmP.sendCommand( SET_FRAME_RATE, rate))
+    {
+        printf( "set to %2uHz.\r\n", rate);
+    }
+    else
+    {
+        printf( "command failed.");
+        tfmP.printStatus( false);
+    }
+}
+
+void saveSettings()
+{
+    printf( "Lidar device settings ");
+    if( tfmP.sendCommand( SAVE_SETTINGS, 0))
+    {
+        printf( "saved.\r\n");
+    }
+    else
+    {
+        printf( "not saved.");
+        tfmP.printStatus( false);
+    }
+}
+
+/*  - - -   End of useful Lidar commands   - - - -   */
+
+void setup()
+{
+    Serial.begin( 115200);   // Intialize terminal serial port
+    delay(20);               // Give port time to initalize
+    printf_begin();          // Initialize printf.
+    printf("\r\nTFMPlus Device Library - 09MAR2019\r\n");  // say 'hello'
+
+    Serial2.begin( 115200);  // Initialize TFMPLus device serial port.
+    delay(20);               // Give port time to initalize
+    tfmP.begin( &Serial2);   // Initialize device library object and...
+                             // pass device serial port to the object.
+
+    firmwareVersion();       // send a few commands
+    frameRate(20);
+    //saveSettings();
+    //factoryReset();
+
+    loopCount = 0;         // Reset loop counter.
+    tfDist = 0;            // Clear device data variables.
+    tfFlux = 0;
+    tfTemp = 0;
+
+    delay(500);            // And wait for half a second.
+}
+
+// Use the 'getData' function to pass back device data.
 void loop()
 {
-  // Excample of using the 'getData' function to pass back data. Show the
-  // loop counmt and the range (distance), and every twenty loops, show
-  // all the data.  Not the formula for decoding temperature (C° = (t/8) - 256).
-  printf( "Loop:%02u", loopCount);                     // Display the loop count.
-  if( tfmP.getData( tfDist, tfFlux, tfTemp)) // Get data from the device.
+  //if( tfmP.sendCommand( TRIGGER_DETECTION, 0));
+  //delay(20);
+
+  // 1. On each loop, try up to five tries to get a good data frame.
+  for( uint8_t fvi = 1; fvi < 6; ++fvi)
   {
-    printf( " Dist:%04u", tfDist);          // Display the distance.
-    if( loopCount >= 20)                    // And every twenty loops...
-    {
-      printf( " Flux:%05u", tfFlux);        // display signal strength...
-      uint8_t tfTempC = uint8_t(( tfTemp / 8) - 256); // decode temperature...
-      printf( " Temp:% 2uC", tfTempC);      // display temp as °C...
-      uint8_t tfTempF = uint8_t( tfTempC * 9.0 / 5.0) + 32.0;
-      printf( "% 3uF", tfTempF);            // and again as °F.
-      loopCount = 0;                        // Reset loop counter.
-    }
+      printf( "Loop:%02u", loopCount);           // Display the loop count.
+      if( tfmP.getData( tfDist, tfFlux, tfTemp)) // Get data from the device.
+      {
+        printf( " Dist:%04u", tfDist);           // Display the distance.
+        break;                                   // Escape this sub-loop
+      }
+      else                        // If the command fails...
+      {
+        tfmP.printStatus( true);  // display the error.
+      }
   }
-  else tfmP.printStatus();
+
+  // 2. Every twenty loops, show the two additional values.
+  if( loopCount >= 20)
+  {
+    // Display signal strength in arbitrary units.
+    printf( " Flux:%05u", tfFlux);
+
+    // Decode temperature data and display as degrees Centigrade.
+    uint8_t tfTempC = uint8_t(( tfTemp / 8) - 256);
+    printf( " Temp:% 2uC", tfTempC);
+
+    // Convert temperature and display as degrees Farenheit.
+    uint8_t tfTempF = uint8_t( tfTempC * 9.0 / 5.0) + 32.0;
+    printf( "% 3uF", tfTempF);
+
+    loopCount = 0;                   // Reset loop counter.
+  }
+
   printf("\r\n");  // Send CR/LF to terminal
-  loopCount++;     // AAdvance the loop counter
+  loopCount++;
   delay(10);       // Delay to match the 100Hz data frame rate.
 }
